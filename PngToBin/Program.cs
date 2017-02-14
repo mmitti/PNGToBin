86a913316c9c807411eb9281424de392ba40db18
mmitti <dev@mail.mmitti.info>
2017-02-15 08:06:16 +0900

    initial commit

---
+++ PngToBin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PngToBin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String src = args[0];
+            Path.GetFileName(src);
+            ;
+            String dst = Path.GetDirectoryName(src) + Path.DirectorySeparatorChar+Path.GetFileNameWithoutExtension(src) + ".bin";
+            FileStream fs = new FileStream(dst, FileMode.Create);
+            Bitmap b = new Bitmap(src);
+             for (int y = 0; y < b.Height; y++)
+                {
+                for (int x = 0; x < b.Width; x++)
+                {
+                    Color c = b.GetPixel(x, y);
+                    byte[] tmp = new byte[4];
+                    tmp[3] = c.A;
+                    tmp[2] = c.R;
+                    tmp[1] = c.G;
+                    tmp[0] = c.B;
+                    fs.Write(tmp, 0, 4);
+                }
+            }
+            fs.Close();
+        }
+    }
+}
